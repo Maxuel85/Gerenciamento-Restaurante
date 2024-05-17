2cecc22f3304418313b98aad406b3bd1037b84f6
Maxuel <maxegi12@gmail.com>
2024-05-17 19:24:02 -0300

    Segundo commit

--- GerenciamentoRestaurante.py
+++ GerenciamentoRestaurante.py
@@ -1,2 +1,23 @@
-def gerenciamento():
+def restaurante():
+    while True:
+        print("\nGerenciamento do Restaurante: ")
+        print("1: Visualizar Produtos em Estoque")
+        print("2: Substituir Produtos")
+        print("3: Adicionar Novos Produtos")
+        print("4: Remover Produtos")
+        print("5: Consultar Detalhes de um Produto")
+        print("6: Sair do Sistema")
+
+        try:
+            menu = int(input("Digite o número do menu: "))
+
+            if menu == 6:
+                print("Encerrando gerenciador, Até Logo!")
+                break
+            else:
+                print("Essa opção não existe no menu, tente novamente")
+        except ValueError:
+            print("Insira uma opção válida")
+
+restaurante()
     
